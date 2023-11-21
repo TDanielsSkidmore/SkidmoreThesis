efadded02d8222275a649111132fd6cf54aecf5d
Daniels <tdaniels@mccarthy.cs.skidmore.edu>
2023-11-21 12:50:03 -0500

    test num1

--- MyImplementation/objectivenessModelPredict.py
+++ MyImplementation/objectivenessModelPredict.py
@@ -5,17 +5,17 @@ import ModelFunctions
 import testBBRData
 
 
-(training_data, training_labels), (testing_data, testing_labels), (validation_data, validation_labels) = ModelFunctions.splitImages()
-
+(training_data, training_labels), (testing_data, testing_labels), (validation_data, validation_labels) = ModelFunctions.splitImages(skip_data=760)
 
 
 
 def seeObjectivenessPrediction(testing_data, testing_labels, model_name = "objectivness_score_model.hs"):
     model = keras.models.load_model(model_name)
+    print(testing_data.size)
     y_pred = model.predict(testing_data)
     print(y_pred)
     for i in range(len(testing_data)):
-        print("Predicted value is: "+str(y_pred)+" and true value is: "+str(testing_labels[i]))
+        print("Predicted value is: "+str(y_pred[i])+" and true value is: "+str(testing_labels[i]))
         cv.imshow('test', testing_data[i])
         cv.waitKey(0)        
         cv.destroyAllWindows()
