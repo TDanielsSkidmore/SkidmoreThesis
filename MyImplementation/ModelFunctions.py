6f18d09665765965841e46c4780a68fa13b73df5
Daniels <tdaniels@mccarthy.cs.skidmore.edu>
2023-11-22 08:45:19 -0500

    test13

--- MyImplementation/ModelFunctions.py
+++ MyImplementation/ModelFunctions.py
@@ -53,7 +53,8 @@ def load_data():
     return (training_data, training_labels), (testing_data, testing_labels), (validation_data, validation_labels)
 
 
-
+# - objectiveness score - goes through each image and segments it out into many while computing the lable , how
+# much of the object is in the segement which is computed using the ground truth bounding box
 def splitImages(filter_zero_lables=True, skip_data=0):
      # lists for data and labels
     images = []
@@ -82,17 +83,17 @@ def splitImages(filter_zero_lables=True, skip_data=0):
         for i in range(segment_dim_height, H ,one_sixth_image_height):
             for j in range(segment_dim_width, W, one_sixth_image_width  ) :
                 temp_image = image[i-segment_dim_height:i,j-segment_dim_width:j]
-
+                #coordinates of segment
                 x1 = j - segment_dim_width
                 y1 = i - segment_dim_height
                 x2 = j
                 y2 = i
-                
+                # coordinates of intersection of segment and bonding box (may be no intersection which is checked below)
                 x1_intersect = max(x1,x1_bbox)
                 y1_intersect = max(y1,y1_bbox)
                 x2_intersect = min(x2, x2_bbox)
                 y2_intersect = min(y2, y2_bbox)
-
+                
                 if (x1_intersect>x2) or (y1_intersect>y2) or (x2_intersect<x1) or (y2_intersect<y1):
                     objectiveness_label = 0
                 else:
@@ -102,35 +103,12 @@ def splitImages(filter_zero_lables=True, skip_data=0):
                 temp_image = (temp_image - 127.5) / 127.5
                 images.append(temp_image)
                 labels.append(objectiveness_label)
-
-        # segment_dim_height =  math.floor( (3 *H) / 4)
-        # segment_dim_width = math.floor( (3 * W) / 4)
-        # for i in range(segment_dim_height, H ,one_sixth_image_height):
-        #     for j in range(segment_dim_width, W, one_sixth_image_width) :
-        #         temp_image = image[i-segment_dim_height:i,j-segment_dim_width:j]
-
-        #         x1 = j - segment_dim_width
-        #         y1 = i - segment_dim_height
-        #         x2 = j
-        #         y2 = i
-                
-        #         x1_intersect = max(x1,x1_bbox)
-        #         y1_intersect = max(y1,y1_bbox)
-        #         x2_intersect = min(x2, x2_bbox)
-        #         y2_intersect = min(y2, y2_bbox)
-
-        #         if (x1_intersect>x2) or (y1_intersect>y2) or (x2_intersect<x1) or (y2_intersect<y1):
-        #             objectiveness_label = 0
-        #         else:
-        #             objectiveness_label = ( (x2_intersect - x1_intersect) * (y2_intersect - y1_intersect) ) / ( (x2_bbox-x1_bbox) * (y2_bbox - y1_bbox) )
-
-        #         temp_image = cv.resize(temp_image, (256,256))
-        #         temp_image = (temp_image - 127.5) / 127.5
-        #         images.append(temp_image)
-        #         labels.append(objectiveness_label)
+    # The data is biased toward some lables depending on the size of the segment, this gets rid of the bias in the data
     if filter_zero_lables:
         images,labels = filter_data(images,labels)
+    # Simular data are also consecutive in the data, this shuffles it
     images, labels = shuffleData(images,labels)
+    # break into train segment and test
     training_data = np.array(images[:round(len(images)*0.8)])
     training_labels = np.array(labels[:round(len(images)*0.8)])
     testing_data = np.array(images[round(len(images)*0.8):round(len(images)*0.9)])
@@ -140,7 +118,7 @@ def splitImages(filter_zero_lables=True, skip_data=0):
     print("Finished loading data")
     return (training_data, training_labels), (testing_data, testing_labels), (validation_data, validation_labels)
 
-
+# - objectiveness score
 def filter_data(images,labels):
     distribution = [0,0,0,0,0,0,0,0,0,0,0]
     for i in range(len(labels)):
@@ -148,17 +126,16 @@ def filter_data(images,labels):
         distribution[amount]+=1
     distributionSorted = copy.deepcopy(distribution)
     distributionSorted.sort()
-    medianAmount = distributionSorted[4]
+    medianAmount = distributionSorted[3]
     labelsToReturn = []
     imagesToReturn = []
     for i in range(len(images)):
         amount = round(labels[i]*10)
-        if amount !=4 and amount != 5 and amount != 6:
-            if distribution[amount] > medianAmount:
-                distribution[amount]-=1
-            else:
-                labelsToReturn.append(labels[i])
-                imagesToReturn.append(images[i])
+        if distribution[amount] > medianAmount:
+            distribution[amount]-=1
+        else:
+            labelsToReturn.append(labels[i])
+            imagesToReturn.append(images[i])
     return imagesToReturn,labelsToReturn
 
         
@@ -176,7 +153,7 @@ def IoU(bbox_true, bbox_pred):
 
     iou = intersection_area / float(true_area + bbox_area - intersection_area)
     return iou
-
+# - objectiveness score
 def shuffleData(list_data,list_labels):
     listOfIndexes = []
     dataToReturn = []
--- MyImplementation/objectivenessModelPredict.py
+++ MyImplementation/objectivenessModelPredict.py
@@ -8,7 +8,7 @@ import testBBRData
 (training_data, training_labels), (testing_data, testing_labels), (validation_data, validation_labels) = ModelFunctions.splitImages(skip_data=700)
 
 
-
+# - objectiveness score - make prediction for the test data (I am skipping most of the data so I can load it locally, all is actually part of validation (within 90% of data))
 def seeObjectivenessPrediction(testing_data, testing_labels, model_name = "objectivness_score_model.hs"):
     model = keras.models.load_model(model_name)
     y_pred = model.predict(testing_data)
